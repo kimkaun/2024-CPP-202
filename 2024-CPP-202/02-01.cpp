04b3ee44243968a98a027d48362d216e7affbae2
82107 <82107@>
2024-03-21 12:11:25 +0900

    02-01 기본예제

---
+++ 2024-CPP-202/02-01.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+	//cout : Ãâ·Â°´Ã¼, endl : °³Çà°´Ã¼
+	std::cout << "Hello World " << std::endl;
+	return 0;
+}
--- 2024-CPP-202/예제연습.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(void) {
-
-	// 주석 테스트
-	printf("안녕세상아");
-	return 0;
-}
