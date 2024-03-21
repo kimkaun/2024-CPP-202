8a93fca0d2c079261dc86222e219b904be4e560f
82107 <82107@>
2024-03-21 12:12:08 +0900

    02-01 이름공간

--- 2024-CPP-202/02-01.cpp
+++ 2024-CPP-202/02-01.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+//이름공간. std::를 안해도 된다.
+using namespace std;
+
 int main(void) {
 	//cout : 출력객체, endl : 개행객체
-	std::cout << "Hello World " << std::endl;
+	cout << "Hello World " << endl;
 	return 0;
 }
