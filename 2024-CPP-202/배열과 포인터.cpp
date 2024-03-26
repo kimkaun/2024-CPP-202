23f9afe2fac743e6baf27fd3eee719ebe7130bbe
82107 <82107@>
2024-03-26 16:09:48 +0900

    같은 결과 2

--- 2024-CPP-202/배열과 포인터.cpp
+++ 2024-CPP-202/배열과 포인터.cpp
@@ -5,8 +5,7 @@ int main(void) {
 	int arr[4] = { 10, 20, 30, 40 };
 	char str[4] = "abc";
 
-	//주솟값 4바이트 만큼의 차이
 	printf("%d %d", &arr[0], &arr[1]);
-	printf("%d %d", arr+0, arr+1);
+	printf("%d %d", (arr+0), (arr+1));
 	return 0;
 }
