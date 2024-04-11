0bd620fdb324fd01c05ee804c8cce962c803c489
82107 <82107@>
2024-04-11 10:46:18 +0900

    03-01 생성자 오버로딩

--- 2024-CPP-202/03-01 구조체와 클래스.cpp
+++ 2024-CPP-202/03-01 구조체와 클래스.cpp
@@ -7,9 +7,9 @@ class Student {
 public:
 	//  생성자 : 객체가 생성될 때 호출되는 함수
 	// 생성자는 반환형(return type)이 없다.
-	Student(int hakbun, string name) {
-		hakbun_ = hakbun;
-		name_ = name;
+	Student() {
+		hakbun_ = 2222;
+		name_ = "swag";
 	}
 private:
 	int hakbun_;
@@ -18,7 +18,8 @@ private:
 
 int main(void) {
     Student jisu = Student(2202, "나지수");
-	
+	Student swag = Student();
+
 	return 0;
 
 }
