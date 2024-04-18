4e3bac6fc9e3231891194855d57eea575501b770
82107 <82107@>
2024-04-18 11:41:20 +0900

    03-02 생성자, 소멸자 호출시기 확인

--- 2024-CPP-202/03-02생성자와 소멸자.cpp
+++ 2024-CPP-202/03-02생성자와 소멸자.cpp
@@ -12,10 +12,12 @@ public:
 		name_ = new char[length + 1];
 
 		strcpy(name_, name);
+		cout << "생성자호출" << endl;
 	}
 	// 소멸자 : 객체가 소멸될 때(메모리에서 지워질때) 호출되는 함수
 	~Student(void) {
 		delete[] name_;
+		cout << "소멸자 호출 완료" << endl;
 	}
 
 	// 클래스의 멤버를 출력
@@ -30,9 +32,10 @@ private:
 
 int main(void) {
 	Student* stu = new Student(2213, "조수빈");
-	stu->show();
+	cout << "I'm still hungry - 거스 히딩크" << endl;
 
 	delete stu;
+	cout << "It ait't over, till it's over - 요기배라" << endl;
 
 	return 0;
 
