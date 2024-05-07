0699c8fb326614210e2da24e4e07d698875f1beb
82107 <82107@>
2024-05-07 15:53:28 +0900

    03-03 얕은 복사2

--- 2024-CPP-202/03-03 복사 생성자.cpp
+++ 2024-CPP-202/03-03 복사 생성자.cpp
@@ -14,8 +14,15 @@ public:
 		strcpy(name_, name);
 		cout << "생성자호출" << endl;
 	}
+
+	// 복사생성자
+	Student(const Student& rhs) : hakbun_(rhs.hakbun_), name_(rhs.name_) {
+		cout << "복사생성자 호출" << endl;
+	}
+
 	// 소멸자 : 객체가 소멸될 때(메모리에서 지워질때) 호출되는 함수
 	~Student(void) {
+		// TODO : 앝은 복사로 인하여 이미 제거된 공간을 또 제거하려고 하여서 에러 발생
 		delete[] name_;
 		cout << "소멸자 호출 완료" << endl;
 	}
