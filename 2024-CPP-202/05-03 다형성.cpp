a0320e04dcaf9913b2bb0a60c89638a9885626f9
82107 <82107@>
2024-08-20 15:49:11 +0900

    생성자 복습

--- 2024-CPP-202/05-03 다형성.cpp
+++ 2024-CPP-202/05-03 다형성.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 class Animal {		// Animal 클래스 
 public:
+	// 생성자
+	Animal(string name, unsigned int age, int leg_num)
+		: name_(name), age_(age), leg_num_(leg_num) {}	// 멤버변수(매개변수)
+	
 	// 동물이 하는 행동들
 	void walk(void) {
 		cout << "걷다" << endl;
@@ -22,7 +26,7 @@ private:
 };
 
 void main(void) {
-	Animal* animal = new Animal();	// 동적할당
+	Animal* animal = new Animal("요아조비", 8, 2);	// 동적할당
 	animal->walk();	
 	animal->bark();
 	animal->eat();
