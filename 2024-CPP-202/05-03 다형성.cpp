f0b2b003ebf35669215b11e77d4fa485d0b00f6a
82107 <82107@>
2024-08-22 11:17:58 +0900

    다형성(동적바인딩)

--- 2024-CPP-202/05-03 다형성.cpp
+++ 2024-CPP-202/05-03 다형성.cpp
@@ -6,16 +6,19 @@ class Animal {		// Animal Å¬·¡½º
 public:
 	// »ý¼ºÀÚ
 	Animal(string name, unsigned int age, int leg_num)
-		: name_(name), age_(age), leg_num_(leg_num) {}	// ¸â¹öº¯¼ö(¸Å°³º¯¼ö)
-	
+		: name_(name), age_(age), leg_num_(leg_num) {	// ¸â¹öº¯¼ö(¸Å°³º¯¼ö)
+			cout << "ÀÌ¸§ : " << name_<< endl;
+			cout << "³ªÀÌ : " << age_ << endl;
+			cout << "´Ù¸® : " << leg_num_ << endl;
+		}	
 	// µ¿¹°ÀÌ ÇÏ´Â Çàµ¿µé
-	void walk(void) {
+	virtual void walk(void) {
 		cout << "°È´Ù" << endl;
 	}
-	void bark(void) {
+	virtual void bark(void) {
 		cout << "Â¢´Ù" << endl;
 	}
-	void eat(void) {
+	virtual void eat(void) {
 		cout << "¸Ô´Ù" << endl;
 	}
 private:
@@ -25,11 +28,38 @@ private:
 	int leg_num_;
 };
 
+class Dog : public Animal {
+public:
+	Dog(string name, int age, int leg_num, int loyalty)
+		: Animal(name, age, leg_num), loyalty_(loyalty) {	// name_(name) --> ¿¡·¯, dog´Â animal¿¡ ¸â¹öÁ¢±ÙÀ» ¸øÇÔ
+			cout << "Ãæ¼ºµµ" << endl;
+		}
+
+	virtual void bark() {
+		cout << "¿ïÇÁ¿ïÇÁ" << endl;
+	}
+	virtual void eat() {
+		cout << "¿Õ¿Õ" << endl;
+	}
+	virtual void walk() {
+		cout << "­h­h­h­h" << endl;
+	}
+private:
+	int loyalty_;	// Ãæ¼ºµµ
+};
+
 void main(void) {
 	Animal* animal = new Animal("¿ä¾ÆÁ¶ºñ", 8, 2);	// µ¿ÀûÇÒ´ç
 	animal->walk();	
 	animal->bark();
 	animal->eat();
 	delete animal;	// µ¿ÀûÇÒ´ç ÇØÁ¦
+
+	animal = new Dog("¸¶·ç", 5, 2, 100);	// µ¿ÀûÇÒ´ç
+	animal->walk();
+	animal->bark();
+	animal->eat();
+	delete animal;
+
 	
 }
