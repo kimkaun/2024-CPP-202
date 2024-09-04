a5715fa195739b2e668940a7981dc06a886af9af
82107 <82107@>
2024-09-04 14:06:03 +0900

    Kimchi::attack()에 크리티컬 데미지 추가

--- 2024-CPP-202/다형성예제.cpp
+++ 2024-CPP-202/다형성예제.cpp
@@ -7,23 +7,24 @@ using namespace std;
 class Food {
 public:
 	// 생성자
-	Food(int civil, int force, string name, int territory)		
+	Food(int civil, int force, string name, int territory)
 		: civil_(civil), force_(force), name_(name), territory_(territory) {
-		
+
 	}
+
+	virtual ~Food() {}  // 가상 소멸자 추가
+
 	virtual void show() {
+		// Food::show()를 재귀적으로 호출하는 부분 제거
 		cout << "국민 수 : " << civil_ << endl;
 		cout << "군사력 : " << force_ << endl;
 		cout << "이름 : " << name_ << endl;
 		cout << "땅 : " << territory_ << endl;
 	}
-	void attack(Food* target) {
-		target->force_ -= force_;
-		if (target->force_ <= 0)
-			delete target;
-	}
-	virtual void attack(Food* target) = 0;
-// Food 속성
+
+	virtual void attack(Food* target) = 0;  // 순수 가상 함수, 자식 클래스에서 구현해야 함
+
+	// Food 속성
 public:
 	int civil_;			// 국민 수
 	int force_;			// 군사력 : 0이 되면 멸망
@@ -32,22 +33,29 @@ public:
 };
 
 class Kimchi : public Food {
-// 상속받은 것의 생성자
+	// 상속받은 것의 생성자
 public:
-	Kimchi(int civil, int force, string name, int territory, int garlic, int pepper) 
+	Kimchi(int civil, int force, string name, int territory, int garlic, int pepper)
 		: Food(civil, force, name, territory), garlic_(garlic), pepper_(pepper) {
-
-		}
-	void show() {
+		ca_ = 0;
+	}
+	void show() override {
+		Food::show();
 		cout << "마늘 : " << garlic_ << endl;
 		cout << "고추 : " << pepper_ << endl;
 	}
+	// 기본 공격 매 3타마다 크리티컬 데미지
 	void attack(Food* target) override {
-		target->force_ -= this->force_ * 2;
+		ca_++;
+		if (ca_ % 3 == 0)
+			target->force_ -= this->force_ * 2;
+		else
+			target->force_ -= this->force_;
 	}
 public:
-	int garlic_;	
+	int garlic_;
 	int pepper_;
+	int ca_;		// 기본공격 3타당 크리티컬 어택
 };
 
 class Jelly : public Food {
@@ -56,7 +64,8 @@ public:
 		: Food(civil, force, name, territory), gelatin_(gelatin), sugar_(sugar) {
 
 	}
-	void show() {
+	void show() override {
+		Food::show();
 		cout << "젤라틴 : " << gelatin_ << endl;
 		cout << "설탕 : " << sugar_ << endl;
 	}
@@ -74,11 +83,11 @@ public:
 		: Food(civil, force, name, territory), milk_(milk), rennet_(rennet) {
 
 	}
-	void show() {
+	void show() override {
 		Food::show();
 		cout << "우유 : " << milk_ << endl;
 		cout << "응고제 : " << rennet_ << endl;
- 	}
+	}
 	void attack(Food* target) override {
 		target->force_ += this->force_ / 3;
 	}
@@ -87,10 +96,10 @@ public:
 	int rennet_;		// 응고제
 };
 
-void main(void) {
+int main(void) {
 	Food* player = new Kimchi(15, 95, "갓김치", 86, 100, 100);
 	Food* friends = new Cheese(100, 20, "짜계치", 100, 20, 100);
-	
+
 	while (true) {
 		system("cls");
 		cout << "★ 나 ★" << endl;
@@ -115,6 +124,7 @@ void main(void) {
 		case 1:
 			// TODO : 공격
 			cout << "공격" << endl;
+			player->attack(friends);
 			break;
 		case 2:
 			// TODO : 특수1
