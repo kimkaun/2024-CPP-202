41f0f55cc62d817e03fbc026ca9486d30af78c4a
82107 <82107@>
2024-09-26 11:13:04 +0900

    09-01 enum 적용 후

--- 2024-CPP-202/09-01 enum.cpp
+++ 2024-CPP-202/09-01 enum.cpp
@@ -2,22 +2,30 @@
 
 using namespace std;
 
-void main(void) {
-	// 0 : 한식, 1 : 일식, 2 : 중식
-	int style = 0;
+// 음식 스타일을 나타내는 enum 선언
+// 열거형 정의 
+enum FoodStyle {
+    KOREAN,     // 0: 한식 (0은 디폴트 값이고 값을 주고 싶으면 KOREAN=0으로 한다. )
+    JAPANESE,   // 1: 일식
+    CHINESE     // 2: 중식
+};
 
-	switch (style) {
-	case 0:
-		cout << "김치, 쌈, 비빔밥" << endl;
-		break;
-	case 1: 
-		cout << "초밥, 우동, 타코야끼" << endl;
-		break;
-	case 2:
-		cout << "동파육, 마라타후루" << endl;
-		break;
-	}
+int main(void) {
+    // 0 : 한식, 1 : 일식, 2 : 중식
+    // 열겨형을 사용하면 가독성도 좋아지고, 중간에 값을 유연하게 추가할 수 있다. 
+    int style = KOREAN; // enum 값으로 초기화
 
-	
-	
+    switch (style) {
+    case KOREAN:
+        cout << "김치, 쌈, 비빔밥" << endl;
+        break;
+    case JAPANESE:
+        cout << "초밥, 우동, 타코야끼" << endl;
+        break;
+    case CHINESE:
+        cout << "동파육, 마라탕후루" << endl;
+        break;
+    }
+
+    return 0;
 }
