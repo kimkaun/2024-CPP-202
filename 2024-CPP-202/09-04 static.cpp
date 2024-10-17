807542ee16b7282801f702fcf0c8e0f730d3f9e8
82107 <82107@>
2024-10-17 10:49:02 +0900

    09-04 2반의 정보를 얻기 위해 강제적으로 객체를 생성할 이유는 없다.

---
+++ 2024-CPP-202/09-04 static.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class SW2_2 {
+public:
+	SW2_2(string name) : name_(name) {
+		num_++;
+	}
+	static string get_teacher(void) { return teacher_; }
+	static int get_num(void) { return num_; }
+	string get_name(void) { return name_; }
+private:
+	static string teacher_;	// 담임선생님
+	string name_;
+	static int num_;
+};
+
+string SW2_2::teacher_ = "GD";
+int SW2_2::num_ = 0;
+
+void main(void) {
+	SW2_2* sb = new SW2_2("조수빈");
+	cout << "인원수 : " << SW2_2::get_num() << endl;
+	SW2_2* js = new SW2_2("나지수");
+	cout << "인원수 : " << SW2_2::get_num() << endl;
+
+	delete js;
+	delete sb;
+}
